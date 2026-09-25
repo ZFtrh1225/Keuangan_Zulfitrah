d052ac3cc9807191052e5cf3ed4ef62fa81bc8b6
ZFtrh <zulkiflidunggio22@gmail.com>
2026-09-25 17:57:00 +0700

    Preserve active Apps Script deployment URL

--- js/api.js
+++ js/api.js
@@ -12,7 +12,7 @@
 
   const MT = (window.MT = window.MT || {});
 
-  const API_URL = 'https://script.google.com/macros/s/AKfycbxDgJOZjbs3OU6_KvhaPrHZHT_qQK0qpDElFYlwIe8mLKl-scRYZbEuOnf7twnJziEc7Q/exec';
+  const API_URL = 'https://script.google.com/macros/s/AKfycbzIYA1DtxGBPt2GjB9mO6JxVqeLBYKuuqVIQ5TRawujS2tm1LNQRzgsstSkoaM90uIMLw/exec';
 
   const DEFAULT_TIMEOUT_MS = 30000;
   const MAX_RETRIES = 2;
